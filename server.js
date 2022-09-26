963046ced8fe34ee85655f5398389eb980c8abee
Ben Manning <bmanning@me.com>
2022-09-26 13:43:07 -0400

    adding cors extension

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
+const cors = require("cors");
 const db = require("./db");
 const { Post } = require("./models");
 
+app.use(cors());
 app.get("/posts", async (req, res) => {
   const posts = await Post.find({});
   res.json(posts);
